e3438c3499fc072e9fbfbae51097a4a1702bfc2f
Anish Shah <1804417@brunel.ac.uk>
2021-03-23 21:10:13 +0000

    adding files, using sentence count and average sentence length

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import nltk
 import re
 import pandas as pd
+import numpy as np
 import itertools as it
+import textstat
+from scipy.sparse import coo_matrix, hstack
 from collections import Counter
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 documents = {'originaldoc_1': [1], 'originaldoc_2': [2], 'originaldoc_3': [3], 'originaldoc_4': [4], 'originaldoc_5': [5]
          }
 input_document = "altered3_doc1"
-def paragraphs (fileobj, seperator = '/n'):
+def paragraphs (file, seperator = '/n'):
     #iterate a fileobj by paragraph
     lines = []
-    for line in fileobj:
+    for line in file:
         if line == seperator and lines:
             yield ''.join(lines)
             lines = []
@@ -27,17 +29,25 @@ def reading_in_files(filenames):
         for para, group in zip(paras, it.cycle(paragraph_lists)):
             group.append(para)
     return paragraph_lists
-    # return '\n'.join(strings)
 def remove_numbers(text):
     text_nonum = re.sub(r'\d+', '', text)
     return text_nonum
 
 def tagmaker(splitdocument,paragraphnum):
     tempnum = paragraphnum
+    col = []
+    paragraphnumrow = []
+    sentencenumdata = []
+    wordcount = []
+
     paralist = []
     l = 0
     document_taglist = []
     while l<= tempnum:
+        col.append(0)
+        paragraphnumrow.append(l)
+        sentencenumdata.append(textstat.sentence_count(splitdocument[l]))
+        wordcount.append((len(splitdocument[l]))/(textstat.sentence_count(splitdocument[l])))
         plaglabel = "paragraph" + str(l+1)
         paralist.append(plaglabel)
         l = l+1
@@ -54,9 +64,19 @@ def tagmaker(splitdocument,paragraphnum):
                 tagstring = tagstring + j + " "
             elif j == 'NNP':
                 tagstring = tagstring + j + " "
+            elif j == 'JJ':
+                tagstring = tagstring + j + " "
+            elif j == 'VB':
+                tagstring = tagstring + j + " "
+            elif j == 'VBD':
+                tagstring = tagstring + j + " "
         document_taglist.append(tagstring)
         #print(tagstring)
-    return paralist,document_taglist
+    npcol = np.array(col)
+    npparagraphrow = np.array(paragraphnumrow)
+    npsentencedata = np.array(sentencenumdata)
+    npwordData = np.array(wordcount)
+    return paralist,document_taglist,npcol,npparagraphrow,npsentencedata,npwordData
 
 for files,authornumber in documents.items():#for each file and authornumber in the dictionary split up as such
     docs = reading_in_files(files) #chunk file into paragraphs
@@ -68,18 +88,25 @@ for files,authornumber in documents.items():#for each file and authornumber in t
     for doc in inputDoc[0]:#going through the input document
         inputParanumber = doc.count("\n")#counts the number of paragraphs
         input_paragraphsplit = doc.split("\n")#splits the document into paragraphs
-        inputparalist, tag_input_list = tagmaker(input_paragraphsplit, inputParanumber)
+        inputparalist, tag_input_list,npinputcol,npinputpara,npinputsentdata,npinputwordcount = tagmaker(input_paragraphsplit, inputParanumber)
         #retrieves the paragraph labels and tags
         input_vectorizer = TfidfVectorizer()#creates a tfidf vectorizer
         inputTfidf = input_vectorizer.fit_transform(tag_input_list)#fits the vectorizer to the tags
-
+        inputsentencematrix = coo_matrix((npinputsentdata,(npinputpara,npinputcol)), shape=((inputParanumber+1),1))
+        inputwordmatrix = coo_matrix((npinputwordcount,(npinputpara,npinputcol)),shape=((inputParanumber+1),1))
+        tempinputmatrix = hstack([inputTfidf,inputsentencematrix])
+        inputmatrix = hstack([tempinputmatrix,inputwordmatrix])
     for i in docs[0]:#for each document in the docs list
         paranumber = i.count("\n") #counts the number of paragraphs
         paragraphsplit = i.split("\n")#splits the documents into paragraphsplit list
-        paralist, tag_para_list = tagmaker(paragraphsplit, paranumber)#retrieves the paragraph labels and tags
+        paralist, tag_para_list,npdoccol, npparagraphnum, npsentdata, npdocwordcount = tagmaker(paragraphsplit, paranumber)#retrieves the paragraph labels and tags
         vectorizer = TfidfVectorizer() #creates a tfidf vectorizer
         docTfidf = vectorizer.fit_transform(tag_para_list) #fits  the vectorizer to the tags
-        csim = cosine_similarity(inputTfidf, docTfidf)
+        docsentencematrix = coo_matrix((npsentdata,(npparagraphnum,npdoccol)), shape=((paranumber+1),1))
+        docwordmatrix = coo_matrix((npdocwordcount,(npparagraphnum, npdoccol)),shape=((paranumber+1),1))
+        tempdocmatrix = hstack([docTfidf,docsentencematrix])
+        docmatrix = hstack([tempdocmatrix,docwordmatrix])
+        csim = cosine_similarity(inputmatrix, docmatrix)
         #calculates the cosine similarity between input document and document from corpus
         csimdataframe = pd.DataFrame(csim,index = (inputparalist), columns = (paralist))
         #creates pandas dataframe to display cosine similarity
@@ -89,7 +116,7 @@ for files,authornumber in documents.items():#for each file and authornumber in t
             highvalue = max(values)#calculates the highest similarity between the paragraphs
             percentagesum = percentagesum + highvalue #adds the highest similarity to the percentage sum
             for colIter in range(0,len(values)):#from 0 to the number of paragraphs in corpus document
-                if values[colIter] >= 0.999:
+                if values[colIter] >= 0.99:
                     print(" ")
                     output_result = str(inputparalist[rowIter] + " in the assessed document has a strong match to "+
                                         paralist[colIter] + " in the original document " + files)
@@ -97,16 +124,11 @@ for files,authornumber in documents.items():#for each file and authornumber in t
                     print(output_result)
                     print(" ")
                     print(inputparalist[rowIter] + " of assessed doc :\n" + input_paragraphsplit[rowIter])
-                    print(" ")
                     print(paralist[colIter] + " of " + files + " :\n" + paragraphsplit[colIter])
                     print(" ")
-        print("Average similarity is :" + (str((percentagesum/len(csim))*100)))#displays the average similarity
+        print("Maximum similarity is :" + (str((percentagesum/len(csim))*100)))#displays the average similarity
         print(" ")
         print(csimdataframe)#displays the cosine similarity matrix
         print("***********")
 
-        #print(paranumber)
 
-    #print(doc[0])
-    # paranumber = doc[1].count("\n")
-    # print(paranumber)
